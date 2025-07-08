ff62b16817137ad1a685d7243ad754ce3a304cec
Sir Loft <bbot69964@gmail.com>
2025-07-08 09:04:40 +0300

    Update loftxmd-github.js

--- loftxmd/loftxmd-github.js
+++ loftxmd/loftxmd-github.js
@@ -1,8 +1,5 @@
 const moment = require('moment-timezone');
 const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
-
 
 async function githubCommand(sock, chatId, message) {
   try {
@@ -11,23 +8,22 @@ async function githubCommand(sock, chatId, message) {
     const json = await res.json();
 
     let txt = `*乂  *LOFT-XMD* 🐛  乂*\n\n`;
-    txt += `✩  *Name* : ${json.name}\n`;
-    txt += `✩  *Watchers* : ${json.watchers_count}\n`;
-    txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
-    txt += `✩  *Last Updated* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
-    txt += `✩  *URL* : ${json.html_url}\n`;
-    txt += `✩  *Forks* : ${json.forks_count}\n`;
-    txt += `✩  *Stars* : ${json.stargazers_count}\n\n`;
+    txt += `✩  *Name*: ${json.name}\n`;
+    txt += `✩  *Watchers*: ${json.watchers_count}\n`;
+    txt += `✩  *Size*: ${(json.size / 1024).toFixed(2)} MB\n`;
+    txt += `✩  *Last Updated*: ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
+    txt += `✩  *URL*: ${json.html_url}\n`;
+    txt += `✩  *Forks*: ${json.forks_count}\n`;
+    txt += `✩  *Stars*: ${json.stargazers_count}\n\n`;
     txt += `*LOFT-XMD* 🐛`;
 
-    // Use the local asset image
-    const imgPath = path.join(__dirname, '../assets/bot_image.jpg');
-    const imgBuffer = fs.readFileSync(imgPath);
-
-    await sock.sendMessage(chatId, { image: imgBuffer, caption: txt }, { quoted: message });
+    await sock.sendMessage(chatId, { 
+      image: { url: 'https://raw.githubusercontent.com/smash-bot/Loft-xmd/main/loft-image/smash.jpeg' }, 
+      caption: txt 
+    }, { quoted: message });
   } catch (error) {
     await sock.sendMessage(chatId, { text: '❌ Error fetching repository information.' }, { quoted: message });
   }
 }
 
-module.exports = githubCommand; 
+module.exports = githubCommand;
